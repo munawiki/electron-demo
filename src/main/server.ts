c7655e7f5f2175d5cfe48fff32d5d587d8575cbc
munawiki <munawiki@gmail.com>
2023-06-04 20:56:18 +0900

    Add cors and socket.io to server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,33 +1,31 @@
-// import cors from 'cors'
-// import { Server } from 'socket.io'
-// import express from 'express'
+import cors from 'cors'
+import { Server } from 'socket.io'
+import express from 'express'
 
-// const server = express()
+const server = express()
 
-// server.use(
-//   cors({
-//     origin: '*',
-//     methods: ['GET', 'POST'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-//   })
-// )
+server.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+)
 
-// const httpServer = server.listen(3000, () => {
-//   console.log('Server is running on port 3000')
-// })
+const httpServer = server.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
 
-// const io = new Server(httpServer)
+const io = new Server(httpServer)
 
-// io.on('connection', (socket) => {
-//   console.log('a user connected')
+io.on('connection', (socket) => {
+  console.log('a user connected')
 
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected')
-//   })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 
-//   socket.on('chat message', (msg) => {
-//     io.emit('message', msg)
-//   })
-// })
-
-export {}
+  socket.on('chat message', (msg) => {
+    io.emit('message', msg)
+  })
+})
