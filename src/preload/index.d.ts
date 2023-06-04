188e34396f7d0425a1d61fa5bc3f6ba5ec86e63e
munawiki <munawiki@gmail.com>
2023-06-04 21:24:21 +0900

    Remove unused code in index.d.ts file

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,17 +1,6 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 import { NetworkStatus } from 'src/shared/types'
 
-// extractZip: (filePaths: string): Promise<string[]> =>
-// ipcRenderer.invoke('extract-zip', filePaths),
-// sendSocketMessage: (message: string): void => {
-// socket.emit('chat message', message)
-// },
-// receiveSocketMessage: (callback: (message: string) => void): void => {
-// socket.on('message', (message) => {
-//   callback(message)
-// })
-// }
-
 declare global {
   interface Window {
     electron: ElectronAPI
