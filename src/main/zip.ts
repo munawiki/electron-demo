ad055bfe70db9e0d272b2edefbeb2cdea4f64cce
muna <munawiki@gmail.com>
2023-06-05 09:50:25 +0900

    Refactor zip.ts to use map instead of forEach

--- src/main/zip.ts
+++ src/main/zip.ts
@@ -3,10 +3,7 @@ import AdmZip from 'adm-zip'
 export const extractZip = async (filePath: string): Promise<string[]> => {
   const zip = new AdmZip(filePath)
   const zipEntries = zip.getEntries()
-  const files: string[] = []
-  zipEntries.forEach((zipEntry) => {
-    files.push(zipEntry.entryName)
-  })
+  const files: string[] = zipEntries.map((zipEntry) => zipEntry.entryName)
   zip.extractAllTo('./tmp', true)
   return files
 }
